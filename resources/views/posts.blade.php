7c3db9b2dd74355a8f2b55e6d5a8698bfd09b1ba
davitlabadze <labadzedato18@gmail.com>
2021-10-09 12:48:45 +0400

    Make a Post Model and Migration

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -4,7 +4,7 @@
         
     <article class="{{ $loop->even ? 'foobar' : '' }}">
         <h1>
-            <a href="/posts/{{ $post->slug }}">
+            <a href="/posts/{{ $post->id }}">
                 {{ $post->title }}
             </a>
         </h1>
@@ -14,7 +14,6 @@
     </div>
     </article>
     @endforeach
-        
-    
 
 </x-layout>
+
--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ Route::get('/', function () {
    return view('posts', ['posts' => Post::all()]);
 });
 
-Route::get('posts/{post}', function ($slug) {
-  return view('post',['post' => Post::findOrFail($slug)]);
+Route::get('posts/{post}', function ($id) {
+  return view('post',['post' => Post::findOrFail($id)]);
 });
 
