4d5d7d15175a476fbf760591d6d2ec935408566d
davitlabadze <labadzedato18@gmail.com>
2021-10-09 19:28:22 +0400

    Show All Posts Associated With a Category

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Category;
 use App\Models\Post;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Route;
@@ -16,13 +17,18 @@ use Spatie\YamlFrontMatter\YamlFrontMatter;
 |
 */
 
-//A Few Tweaks and Consideration
 Route::get('/', function () {
 
-   return view('posts', ['posts' => Post::all()]);
+  return view('posts', ['posts' => Post::all()]);
 });
 
 Route::get('posts/{post:slug}', function (Post $post) {
   return view('post',['post' => $post]);
+});
+
+Route::get('categories/{category:slug}', function (Category $category) {
+
+  return view('posts', ['posts' => $category->posts]);
+    
 });
 
