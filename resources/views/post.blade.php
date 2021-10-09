3fa3e1e99b66f076a10eb635d9ec259704fb4557
davitlabadze <labadzedato18@gmail.com>
2021-10-09 11:02:44 +0400

    edit post.blade

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,17 +1,15 @@
-@extends('layout')
-
-@section('content')
+<x-layout>
     <article>
         <h1>
             {{ $post->title }} 
         </h1>
-
+    
         <div>
         {!! $post->body !!}
             
         </div>
     </article>
-
-    <a href="/">Go Back</a>
     
-@endsection
+    <a href="/">Go Back</a>
+</x-layout>
+    
