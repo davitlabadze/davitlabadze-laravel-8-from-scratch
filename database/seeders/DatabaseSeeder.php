45739b9bd6b3b5c89f694cc730ec07bbcd138935
davitlabadze <labadzedato18@gmail.com>
2021-10-11 19:09:14 +0400

    Turbo Boost With Factories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,57 +17,19 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
 
-        User::truncate();
-        Post::truncate();
-        Category::truncate();
-
-
-        $user = User::factory()->create();
-
-        $personal = Category::create([
-            'name' => 'Personal',
-            'slug' => 'personal'
-        ]);
-
-        $family = Category::create([
-            'name' => 'Family',
-            'slug' => 'family'
-        ]);
-  
-
-        $work = Category::create([
-            'name' => 'Work',
-            'slug' => 'work'
+        $user = User::factory()->create([
+            'name' => 'davit'
         ]);
 
 
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $personal->id,
-            'title' => 'My Personal Post',
-            'slug' => 'my-personal-post',
-            'excerpt' => '<p>Loream ipsum dolar sit amet.</p>',
-            'body' => '<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Animi sint deleniti vel eligendi minima mollitia. Necessitatibus hic corporis numquam voluptatum animi. Consectetur perspiciatis, molestiae non unde ratione cum tenetur a.</p>',
+        Post::factory()->create([
+            'user_id' => $user->id
         ]);
 
+        //Independent
+        // Post::factory()->create();
 
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $family->id,
-            'title' => 'My Family Post',
-            'slug' => 'my-family-post',
-            'excerpt' => '<p>Loream ipsum dolar sit amet.</p>',
-            'body' => '<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Animi sint deleniti vel eligendi minima mollitia. Necessitatibus hic corporis numquam voluptatum animi. Consectetur perspiciatis, molestiae non unde ratione cum tenetur a.</p>',
-        ]);
 
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $work->id,
-            'title' => 'My Work Post',
-            'slug' => 'my-work-post',
-            'excerpt' => '<p>Loream ipsum dolar sit amet.</p>',
-            'body' => '<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Animi sint deleniti vel eligendi minima mollitia. Necessitatibus hic corporis numquam voluptatum animi. Consectetur perspiciatis, molestiae non unde ratione cum tenetur a.</p>',
-        ]);
-
+    
     }    
 }
