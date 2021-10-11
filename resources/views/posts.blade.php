b9ab0ec1edc9f4556189173182ef53b4faf1bc45
davitlabadze <labadzedato18@gmail.com>
2021-10-12 00:02:39 +0400

    Blade Components and CSS Grids

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -3,19 +3,17 @@
     @include('_posts-header')
 
     <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+        @if ($posts->count())
+           <x-posts-grid :posts="$posts"/>
+        @else
+            <p class="text-center">No Post yet. Please check back later.</p>
+        @endif
         
-        <x-post-featured-card/>
 
-        <div class="lg:grid lg:grid-cols-2">
-            <x-post-card/>
-            <x-post-card/>
-
-        </div>
-
-        <div class="lg:grid lg:grid-cols-3">
+        {{-- <div class="lg:grid lg:grid-cols-3">
             <x-post-card/>
             <x-post-card/>
             <x-post-card/>
-        </div>
+        </div> --}}
     </main>
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Models\Category;
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +17,21 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('posts');
-});
+    return view('posts', ['posts' => Post::latest()->get()]);
+  });
+  
+  Route::get('posts/{post:slug}', function (Post $post) {
+    return view('post',['post' => $post]);
+  });
+  
+  Route::get('categories/{category:slug}', function (Category $category) {
+  
+    return view('posts', ['posts' => $category->posts]);
+      
+  });
+  
+  Route::get('authors/{author:username}', function (User $author) {
+    
+    return view('posts', ['posts' => $author->posts]);
+      
+  });
