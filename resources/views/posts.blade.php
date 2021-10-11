aa178d0230f4123426e5186db9cc271036ad5682
davitlabadze <labadzedato18@gmail.com>
2021-10-11 22:50:32 +0400

    Integrate the design

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,21 @@
+<x-layout>
+
+    @include('_posts-header')
+
+    <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+        
+        <x-post-featured-card/>
+
+        <div class="lg:grid lg:grid-cols-2">
+            <x-post-card/>
+            <x-post-card/>
+
+        </div>
+
+        <div class="lg:grid lg:grid-cols-3">
+            <x-post-card/>
+            <x-post-card/>
+            <x-post-card/>
+        </div>
+    </main>
+</x-layout>
