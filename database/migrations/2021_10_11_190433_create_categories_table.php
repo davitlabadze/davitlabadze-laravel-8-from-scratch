e4640d991d802b672ddec605c1e5477b62ed2910
davitlabadze <labadzedato18@gmail.com>
2021-10-12 10:19:14 +0400

    Tweaks and Clean-Up

--- database/migrations/2021_10_11_190433_create_categories_table.php
+++ database/migrations/2021_10_11_190433_create_categories_table.php
@@ -15,8 +15,8 @@ class CreateCategoriesTable extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
-            $table->string('name')->unique();
-            $table->string('slug')->unique();
+            $table->string('name');
+            $table->string('slug');
             $table->timestamps();
         });
     }
--- resources/views/components/dropdown.blade.php
+++ resources/views/components/dropdown.blade.php
@@ -6,7 +6,7 @@
     </div>
 
     {{-- links --}}
-    <div x-show="show" class="py-2 absolute bg-gray-100 w-full mt-2 rounded-xl z-50">
+    <div x-show="show" class="py-2 absolute bg-gray-100 w-full mt-2 rounded-xl z-50 overflow-auto max-h-52">
         {{ $slot }}
 
     </div>
