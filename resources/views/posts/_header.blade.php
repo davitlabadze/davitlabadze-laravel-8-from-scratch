ab1946619d75074b3c7328678b351c3dc0e16e8e
davitlabadze <labadzedato18@gmail.com>
2021-10-13 18:13:12 +0400

    refactor: Merge Category and Search Queries

--- resources/views/posts/_header.blade.php
+++ resources/views/posts/_header.blade.php
@@ -37,7 +37,11 @@
 
         <!-- Search -->
         <div class="relative flex lg:inline-flex items-center bg-gray-100 rounded-xl px-3 py-2">
-            <form method="GET" action="#">
+            <form method="GET" action="/?{{ request()->getQueryString() }}">
+                @if (request('category'))
+                    <input type="hidden" name="category" value="{{ request('category') }}">
+                    
+                @endif
                 <input 
                 type="text" 
                 name="search" 
