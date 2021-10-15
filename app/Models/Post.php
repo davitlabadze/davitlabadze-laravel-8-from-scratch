a5208315d4b58e9585f95741cb4c5b5f1e4f3e98
davitlabadze <labadzedato18@gmail.com>
2021-10-15 19:48:12 +0400

    refactor: Make the Comments Section Dynamic

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -39,6 +39,11 @@ class Post extends Model
         
     }
 
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
+
     public function category()
     {
         return $this->belongsTo(Category::class);
--- resources/views/components/post-comment.blade.php
+++ resources/views/components/post-comment.blade.php
@@ -1,17 +1,21 @@
+@props(['comment'])
+
 <article class="flex bg-gray-100 border border-gray-200 p-6 rounded-xl space-x-4">
     <div class="flex-shrink-0">
-        <img src="https://i.pravatar.cc/100" alt="" class="rounded-xl">
+        <img src="https://i.pravatar.cc/100?u={{ $comment->id }}" alt="" class="rounded-xl">
     </div>
 
     <div>
         <header class="mb-4">
-            <h3 class="font-bold">Marry</h3>
+            <h3 class="font-bold">{{ $comment->author->username }}</h3>
             <p class="text-xs">posted 
-                <time>8 mounths ago</time>
+                <time>{{ $comment->created_at }}</time>
             </p>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat facere adipisci soluta, enim molestias dolorem excepturi vero ad officia impedit quo, natus nisi quis velit dolores totam sapiente, tenetur dolor!
-            </p>
+          
         </header>
+
+        <p>
+            {{ $comment->body}}
+         </p>
     </div>
 </article>
