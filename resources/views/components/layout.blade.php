326a8f5f2e620ed5f425193344f6d7a77cef6e71
davitlabadze <labadzedato18@gmail.com>
2021-10-15 14:37:34 +0400

    feat: Login and Logout

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -15,9 +15,20 @@
                 </a>
             </div>
 
-            <div class="mt-8 md:mt-0">
-                <a href="/" class="text-xs font-bold uppercase">Home Page</a>
+            <div class="mt-8 md:mt-0 flex items-center">
+                @auth
 
+                    <span class="text-xs font-bold uppercase">Welcome, {{ auth()->user()->name }}</span>
+
+                    <form action="/logout" method="POST" class="text-xs font-semiblod text-blue-500 ml-6">
+                        @csrf
+
+                        <button type="submit">Log Out</button>
+                    </form>
+                @else
+                    <a href="/register" class="text-xs font-bold uppercase">Register</a>
+
+                @endauth
                 <a href="#" class="bg-blue-500 ml-3 rounded-full text-xs font-semibold text-white uppercase py-3 px-5">
                     Subscribe for Updates
                 </a>
