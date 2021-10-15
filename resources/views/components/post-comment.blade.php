3b05d9ad1ec49073dfd3508e1f5adb9e3ba495bd
davitlabadze <labadzedato18@gmail.com>
2021-10-15 17:23:27 +0400

    style: Write the Markup for a Post Comment

---
+++ resources/views/components/post-comment.blade.php
@@ -0,0 +1,17 @@
+<article class="flex bg-gray-100 border border-gray-200 p-6 rounded-xl space-x-4">
+    <div class="flex-shrink-0">
+        <img src="https://i.pravatar.cc/100" alt="" class="rounded-xl">
+    </div>
+
+    <div>
+        <header class="mb-4">
+            <h3 class="font-bold">dato</h3>
+            <p class="text-xs">posted 
+                <time>8 mounths ago</time>
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat facere adipisci soluta, enim molestias dolorem excepturi vero ad officia impedit quo, natus nisi quis velit dolores totam sapiente, tenetur dolor!
+            </p>
+        </header>
+    </div>
+</article>
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,11 +1,4 @@
-<!doctype html>
-
-<title>Laravel From Scratch Blog</title>
-<link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
-<link rel="preconnect" href="https://fonts.gstatic.com">
-<link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap" rel="stylesheet">
-
-<body style="font-family: Open Sans, sans-serif">
+<x-layout>
     <section class="px-6 py-8">
         <nav class="md:flex md:justify-between md:items-center">
             <div>
@@ -69,33 +62,20 @@
                         {!! $post->body !!}
                     </div>
                 </div>
+
+                <section class="col-span-8 col-start-5 mt-10 space-y-6">
+                    <x-post-comment/>
+                    <x-post-comment/>
+
+                    <x-post-comment/>
+
+                    <x-post-comment/>
+                    <x-post-comment/>
+                    <x-post-comment/>
+
+                </section>
             </article>
         </main>
-
-        <footer class="bg-gray-100 border border-black border-opacity-5 rounded-xl text-center py-16 px-10 mt-16">
-            <img src="/images/lary-newsletter-icon.svg" alt="" class="mx-auto -mb-6" style="width: 145px;">
-            <h5 class="text-3xl">Stay in touch with the latest posts</h5>
-            <p class="text-sm">Promise to keep the inbox clean. No bugs.</p>
-
-            <div class="mt-10">
-                <div class="relative inline-block mx-auto lg:bg-gray-200 rounded-full">
-                    <form method="POST" action="#" class="lg:flex text-sm">
-                        <div class="lg:py-3 lg:px-5 flex items-center">
-                            <label for="email" class="hidden lg:inline-block">
-                                <img src="/images/mailbox-icon.svg" alt="mailbox letter">
-                            </label>
-
-                            <input id="email" type="text" placeholder="Your email address"
-                                class="lg:bg-transparent pl-4 focus-within:outline-none">
-                        </div>
-
-                        <button type="submit"
-                            class="transition-colors duration-300 bg-blue-500 hover:bg-blue-600 mt-4 lg:mt-0 lg:ml-3 rounded-full text-xs font-semibold text-white uppercase py-3 px-8">
-                            Subscribe
-                        </button>
-                    </form>
-                </div>
-            </div>
-        </footer>
     </section>
-</body>
+</x-layout>
+
