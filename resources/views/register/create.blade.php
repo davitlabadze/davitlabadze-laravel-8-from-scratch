9e1ad7be3868c077ee263efe5957dc391ba855fa
davitlabadze <labadzedato18@gmail.com>
2021-10-15 13:44:20 +0400

    feat: Failed Validation and Old Input Data

--- resources/views/register/create.blade.php
+++ resources/views/register/create.blade.php
@@ -14,8 +14,14 @@
                         type="text"
                         name="name"
                         id="name"
+                        value="{{ old('name') }}"
                         required
                     >
+
+                    @error('name')
+                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                    @enderror
+
                 </div>
 
 
@@ -29,8 +35,14 @@
                         type="text"
                         name="username"
                         id="username"
+                        value="{{ old('username') }}"
                         required
                     >
+
+                    @error('username')
+                    <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                    @enderror
+
                 </div>
 
                 <div class="mb-6">
@@ -43,11 +55,15 @@
                         type="email"
                         name="email"
                         id="email"
+                        value="{{ old('email') }}"
                         required
                     >
+
+                    @error('email')
+                    <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                    @enderror
+
                 </div>
-
-
 
                 <div class="mb-6">
                     <label class="block mb-2 uppercase font-bold text-xs text-gray-700" 
@@ -61,6 +77,12 @@
                         id="password"
                         required
                     >
+
+                    @error('password')
+                    <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                    @enderror
+
+
                 </div>
                 <div class="mb-6">
                     <button type="submit" 
