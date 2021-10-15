b123548a62707fd3f63fd7c6bff26a1a7106e1d7
davitlabadze <labadzedato18@gmail.com>
2021-10-15 16:37:53 +0400

    feat: Login Page

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -3,9 +3,45 @@
 namespace App\Http\Controllers;
 
 
+use Illuminate\Auth\Events\Validated;
+use Illuminate\Validation\ValidationException;
 
 class SessionsController extends Controller
 {
+
+    public function create()
+    {
+        return view('sessions.create');
+    }
+
+    public function store()
+    {
+        $attributes = request()->validate([
+          'email' => 'required|email',
+          'password' => 'required'
+        ]);
+
+
+        if (auth()->attempt($attributes)) {
+
+            session()->regenerate();
+            
+            return redirect('/')->with('success', 'Welcome Back!');
+        }
+
+        throw ValidationException::withMessages([
+            'email' => 'Your provided credentials could not be verofied'
+        ]);
+
+
+
+    //   return back()
+    //   ->withInput()
+    //   ->withErrors(['email' => 'Your provided credentials could not be verofied']);
+    
+    }
+
+
     public function destroy()
     {
         auth()->logout();
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -27,6 +27,7 @@
                     </form>
                 @else
                     <a href="/register" class="text-xs font-bold uppercase">Register</a>
+                    <a href="/login" class="ml-6 text-xs font-bold uppercase">Log In</a>
 
                 @endauth
                 <a href="#" class="bg-blue-500 ml-3 rounded-full text-xs font-semibold text-white uppercase py-3 px-5">
