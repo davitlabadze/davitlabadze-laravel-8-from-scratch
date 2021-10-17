d02607a9d8937a23a93b5ec0c3db11d932976be9
davitlabadze <labadzedato18@gmail.com>
2021-10-17 22:12:16 +0400

    feat: Newsletter Form

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,31 @@ use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\SessionsController;
 use Illuminate\Support\Facades\Route;
 
+Route::post('/newsletter', function () {
+    
+    request()->validate(['email' => 'required|email']);
+
+
+    $mailchimp = new \MailchimpMarketing\ApiClient();
+
+    $mailchimp->setConfig([
+        'apiKey' => config('services.mailchimp.key'),
+        'server' => 'us5'
+    ]);
+
+    try {
+        $response = $mailchimp->lists->addListMember("6bbdf02da4", [
+            "email_address" => request('email'),
+            "status" => "subscribed",
+        ]);
+    } catch (\Exception $e) {
+        throw \Illuminate\Validation\ValidationException::withMessages([
+            'email' => 'This email coul not be added to our newsletter list.'
+        ]);
+    }
+
+    return redirect('/')->with('success', 'You are now signed up for our newsletter!');
+});
 
 Route::get('/', [PostController::class,'index'])->name('home');
 Route::get('posts/{post:slug}',[PostController::class,'show']);
