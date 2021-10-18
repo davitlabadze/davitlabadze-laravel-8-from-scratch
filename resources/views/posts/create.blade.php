a038897b9174666c2a8ba7771a751fc9cd68adb5
davitlabadze <labadzedato18@gmail.com>
2021-10-18 23:07:25 +0400

    style: Extract Form-Specific Blade Components

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -7,149 +7,27 @@
             <form action="/admin/posts" method="post" enctype="multipart/form-data">
                 @csrf
 
-                <div class="mb-6">
-                    <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
-                        for="title" 
-                    >
-                        Title
-                    </label>
-
-
-                    <input class="border border-gray-400 p-2 w-full"
-                        type="text"
-                        name="title"
-                        id="title"
-                        value="{{ old('title') }}"
-                        required
-                    >
-
-
-                    @error('title')
-                        <p class="text-red-500 text xs mt-2">{{ $message }}</p> 
-                    @enderror
-                    </div>
-
-                <div class="mb-6">
-                    <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
-                        for="Slug" 
-                    >
-                        Slug
-                    </label>
-
-
-                    <input class="border border-gray-400 p-2 w-full"
-                        type="text"
-                        name="slug"
-                        id="slug"
-                        value="{{ old('slug') }}"
-                        required
-                    >
-
-
-                    @error('slug')
-                        <p class="text-red-500 text xs mt-2">{{ $message }}</p> 
-                    @enderror
-                </div>
-
-
-                <div class="mb-6">
-                    <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
-                        for="thumbnail" 
-                    >
-                        Thumbnail
-                    </label>
-
-
-                    <input class="border border-gray-400 p-2 w-full"
-                        type="file"
-                        name="thumbnail"
-                        id="thumbnail"
-                        value="{{ old('thumbnail') }}"
-                        required
-                    >
-
-
-                    @error('thumbnail')
-                        <p class="text-red-500 text xs mt-2">{{ $message }}</p> 
-                    @enderror
-                </div>
-
-
-
-
-                <div class="mb-6">
-                    <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
-                        for="excerpt" 
-                    >
-                        Excerpt
-                    </label>
-
-
-                    <textarea class="border border-gray-400 p-2 w-full"
-                        type="textarea"
-                        name="excerpt"
-                        id="excerpt"
-                        required
-                    >{{ old('excerpt') }}</textarea>
-
-
-                    @error('excerpt')
-                        <p class="text-red-500 text xs mt-2">{{ $message }}</p> 
-                    @enderror
-                </div>
-
-
-                <div class="mb-6">
-                    <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
-                        for="body" 
-                    >
-                        Body
-                    </label>
-
-
-                    <textarea class="border border-gray-400 p-2 w-full"
-                        type="textarea"
-                        name="body"
-                        id="body"
-                        required
-                    >{{ old('body') }}</textarea>
-
-
-                    @error('body')
-                        <p class="text-red-500 text xs mt-2">{{ $message }}</p> 
-                    @enderror
-                </div>
-
-                <div class="mb-6">
-                    <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
-                        for="category" 
-                    >
-                        Category
-                    </label>
-
-
-                   <select name="category_id" id="category_id">
-
+                <x-form.input name="title" />
+                <x-form.input name="slug" />
+                <x-form.input name="thumbnail" type="file" />
+                <x-form.textarea name="excerpt" />
+                <x-form.textarea name="body" />
+                <x-form.field>
+                    <x-form.label name="category" />
+                    <select name="category_id" id="category_id">
 
                         @foreach( \App\Models\Category::all() as $category)
-                        <option value="{{ $category->id }}"
-                            {{ old('category_id') == $category->id ? 'selected' : ''}}
-                            >{{ ucwords($category->name) }}
-                        </option>
+                            <option value="{{ $category->id }}"
+                                {{ old('category_id') == $category->id ? 'selected' : ''}}
+                                >{{ ucwords($category->name) }}
+                            </option>
                         @endforeach
 
-
-                       
                    </select>
+                   <x-form.error name="category" />
+                </x-form.field>
 
-
-                    @error('category')
-                        <p class="text-red-500 text xs mt-2">{{ $message }}</p> 
-                    @enderror
-                </div>
-
-                <x-submit-button>Publish</x-submit-button>
-
+                <x-form.button>Publish</x-form.button>
             </form>
        </x-panel>
     </section>
