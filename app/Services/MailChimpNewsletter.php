42525c52230782c09aebe438d77832c12cbbd273
davitlabadze <labadzedato18@gmail.com>
2021-12-09 21:53:10 +0400

    chore: remove construct

--- app/Services/MailChimpNewsletter.php
+++ app/Services/MailChimpNewsletter.php
@@ -2,19 +2,10 @@
 
 namespace App\Services;
 
-use \MailchimpMarketing\ApiClient;
-
 class MailchimpNewsletter implements Newsletter
 {
-
-    public function __construct(ApiClient $client)
-    {
-        //
-    }
-
     public function subscribe(string $email, string $list=null)
     {
-
         $list ??=config('services.mailchimp.lists.subscribers');
 
         return $this->client->lists->addListmember($list, [
@@ -22,6 +13,4 @@ class MailchimpNewsletter implements Newsletter
             'status' => 'subscribed'
         ]);
     }
-
-
 }
