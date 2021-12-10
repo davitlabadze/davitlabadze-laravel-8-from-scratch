a75246f1bc7bee74092be2672800980f423e086b
davitlabadze <labadzedato18@gmail.com>
2021-12-10 23:25:14 +0400

    fix(fix image): missed one asset url

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -61,7 +61,7 @@
                         @csrf
                         <div class="lg:py-3 lg:px-5 flex items-center">
                             <label for="email" class="hidden lg:inline-block">
-                                <img src="/images/mailbox-icon.svg" alt="mailbox letter">
+                                <img src="{{ asset('/images/mailbox-icon.svg') }}" alt="mailbox letter">
                             </label>
 
                             <div>
