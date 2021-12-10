fb65b657fe2b2fef97a241111d67623509e1056e
davitlabadze <labadzedato18@gmail.com>
2021-12-10 22:50:00 +0400

    fix(import): alpinejs

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,12 +4,7 @@
 <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
 <link rel="preconnect" href="https://fonts.gstatic.com">
 <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap" rel="stylesheet">
-<script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
-<style>
-    html{
-        scroll-behavior: smooth;
-    }
-</style>
+<script src="{{ asset('js/app.js') }}" defer></script>
 <body style="font-family: Open Sans, sans-serif">
     <section class="px-6 py-8">
         <nav class="md:flex md:justify-between md:items-center">
