1fdf77134e5323189b24abe49e96b70739e8a56e
davitlabadze <labadzedato18@gmail.com>
2021-12-13 23:00:19 +0400

    chore(category-button):delete comment in code

--- resources/views/components/category-button.blade.php
+++ resources/views/components/category-button.blade.php
@@ -1,5 +1,4 @@
 @props(['category'])
 <a href="{{ 'home' }}?category={{ $category->slug }}"
-{{-- <a href="{{ 'home', ['category' => $category->slug] }}" --}}
     class="px-3 py-1 border border-blue-300 rounded-full text-blue-300 text-xs uppercase font-semibold"
     style="font-size: 10px">{{ $category->name }}</a>
