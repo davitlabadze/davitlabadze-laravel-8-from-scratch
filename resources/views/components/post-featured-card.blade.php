d5f5659f72ed148b6bb96a31dbac5ff47a1c5f6e
davitlabadze <labadzedato18@gmail.com>
2021-12-13 22:44:17 +0400

    chore(post-featured-card):delete comment in code

--- resources/views/components/post-featured-card.blade.php
+++ resources/views/components/post-featured-card.blade.php
@@ -36,8 +36,6 @@ class="transition-colors duration-300 hover:bg-gray-100 border border-black bord
                 <img src="{{ asset('/images/lary-avatar.svg') }}" alt="Lary avatar">
                 <div class="ml-3">
                     <h5 class="font-bold">
-                        {{-- <a href="{{ route('home',['authors' => $post->author->username ])  }}">{{ $post->author->name }}</a> --}}
-
                         <a href="{{ route('home') }}?authors={{ $post->author->username }}">{{ $post->author->name }}</a>
                     </h5>
                 </div>
