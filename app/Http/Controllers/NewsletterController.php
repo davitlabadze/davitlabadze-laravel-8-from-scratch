ffa85a7d6d4bd3c05882a72642eceb7072856c2d
davitlabadze <labadzedato18@gmail.com>
2021-12-16 20:50:33 +0400

    fix(newslettercontroller):remove request store

--- app/Http/Controllers/NewsletterController.php
+++ app/Http/Controllers/NewsletterController.php
@@ -5,14 +5,11 @@ namespace App\Http\Controllers;
 use App\Services\Newsletter;
 use Exception;
 use Illuminate\Validation\ValidationException;
-use App\Http\Requests\StoreNewsletterRequest;
 
 class NewsletterController extends Controller
 {
-    public function __invoke(StoreNewsletterRequest $attribute, Newsletter $newsletter)
+    public function __invoke(Newsletter $newsletter)
     {
-        $attribute->validated();
-
         try {
             $newsletter->subscribe(request('email'));
         } catch (Exception $e) {
