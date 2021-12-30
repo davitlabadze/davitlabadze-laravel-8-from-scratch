a4daab3e699c601d16e8b08a01091e4aed633cb5
davitlabadze <labadzedato18@gmail.com>
2021-12-31 00:08:37 +0400

    fix(category-dropdown and _header): use the route name

--- resources/views/components/category-dropdown.blade.php
+++ resources/views/components/category-dropdown.blade.php
@@ -5,14 +5,14 @@
             <x-icon name="down-arrow" class="absolute pointer-events-none" style="right: 12px;"/>
         </button>
     </x-slot>
-    <x-dropdown-item href="/?{{ http_build_query(request()->except('category','page')) }}"
-        :active="request()->routeIs('home')">All
+    <x-dropdown-item href="{{ route('home', http_build_query(request()->except('category','page'))) }}"
+        :active="request()->routeIs('home') && is_null(request()->getQueryString())">
     </x-dropdown-item>
 
     @foreach ($categories as $category)
 
-        <x-dropdown-item href="/?category={{ $category->slug }}&{{ http_build_query(request()->except('category','page')) }}"
-            :active='request()->is("categories/{$category->slug}")'>
+        <x-dropdown-item href="{{ route('home', ['category' => $category->slug , http_build_query(request()->except('category','page')) ]) }}"
+            :active='request()->fullUrlIs("*?category={$category->slug}*")'>
             {{ ucwords($category->name) }}
         </x-dropdown-item>
     @endforeach
--- resources/views/posts/_header.blade.php
+++ resources/views/posts/_header.blade.php
@@ -11,7 +11,7 @@
         </div>
         <!-- Search -->
         <div class="relative flex lg:inline-flex items-center bg-gray-100 rounded-xl px-3 py-2">
-            <form method="GET" action="/?{{ request()->getQueryString() }}">
+            <form method="GET" action="{{ route('home', request()->getQueryString()) }}">
                 @if (request('category'))
                     <input type="hidden" name="category" value="{{ request('category') }}">
 
